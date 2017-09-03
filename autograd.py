460af6ca3ad7c535c403e81a8721f940060cabad
kvzhao <kelispinor@gmail.com>
2017-09-03 14:44:27 +0800

    autograd examples

--- autograd.py
+++ autograd.py
@@ -25,3 +25,18 @@ print (z.grad)
 print (out.grad)
 
 #print (x.view(x.size()))
+
+x = Variable(torch.randn(1, 10))
+prev_h = Variable(torch.randn(1, 20))
+W_h = Variable(torch.randn(20, 20))
+W_x = Variable(torch.randn(20, 10))
+
+# input to hidden
+i2h = torch.mm(W_x, x.t())
+h2h = torch.mm(W_h, prev_h.t())
+next_h = i2h + h2h
+next_h = next_h.tanh()
+
+next_h.backward(torch.ones(1, 20))
+
+print (next_h)
